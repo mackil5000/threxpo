8343477cc531e6ac597bff33f3f1faef913b2852
mackil5000 <marcusbernroth@gmail.com>
2023-11-17 14:10:16 +0100

    Add 3d objects

--- App.js
+++ App.js
@@ -1,47 +1,40 @@
-import { GLView } from "expo-gl";
-import { Renderer, TextureLoader } from "expo-three";
-import { useEffect } from "react";
+import { GLView } from 'expo-gl';
+import { Renderer } from 'expo-three';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { useEffect } from 'react';
 import {
   AmbientLight,
-  BoxBufferGeometry,
-  Fog,
-  GridHelper,
-  Mesh,
-  MeshStandardMaterial,
   PerspectiveCamera,
   PointLight,
   Scene,
   SpotLight,
-} from "three";
+} from 'three';
+import { Asset } from 'expo-asset';
 
 export default function App() {
   let timeout;
 
   useEffect(() => {
-    // Clear the animation loop when the component unmounts
     return () => clearTimeout(timeout);
   }, []);
 
   const onContextCreate = async (gl) => {
     const { drawingBufferWidth: width, drawingBufferHeight: height } = gl;
-    const sceneColor = 0x6ad6f0;
-
-    // Create a WebGLRenderer without a DOM element
+    const sceneColor = 0x000000;
+    let eyeballs;
     const renderer = new Renderer({ gl });
     renderer.setSize(width, height);
     renderer.setClearColor(sceneColor);
 
     const camera = new PerspectiveCamera(70, width / height, 0.01, 1000);
-    camera.position.set(2, 5, 5);
+    camera.position.set(2, 2, 5);
 
     const scene = new Scene();
-    scene.fog = new Fog(sceneColor, 1, 10000);
-    scene.add(new GridHelper(10, 10));
 
     const ambientLight = new AmbientLight(0x101010);
     scene.add(ambientLight);
 
-    const pointLight = new PointLight(0xffffff, 2, 1000, 1);
+    const pointLight = new PointLight(0xffffff, 1, 1000, 1);
     pointLight.position.set(0, 200, 200);
     scene.add(pointLight);
 
@@ -50,20 +43,41 @@ export default function App() {
     spotLight.lookAt(scene.position);
     scene.add(spotLight);
 
-    const cube = new IconMesh();
-    scene.add(cube);
+    // Load the model
+    const asset = Asset.fromModule(require('./assets/male.obj'));
+    await asset.downloadAsync();
 
-    camera.lookAt(cube.position);
+    const loader = new OBJLoader();
+    loader.load(asset.localUri, (object) => {
+      object.scale.set(1, 1, 1); // Adjust scale if needed
+      // object.rotateY(0.6);
+      scene.add(object);
+      camera.lookAt(object.position);
+      object.traverse((child) => {
+        if (child.isMesh && child.name === 'Group9192') {
+          // name of the eyeballs group
+          // Clone the material to create a unique instance
+          const newMaterial = child.material.clone();
+          child.material = newMaterial;
+          eyeballs = child;
+        }
+      });
+    });
 
-    function update() {
-      cube.rotation.y += 0.05;
-      cube.rotation.x += 0.025;
-    }
+    let n = 0;
+    setInterval(() => {
+      if (eyeballs) {
+        if (n % 2 === 0) {
+          eyeballs.material.color.set(0xff0000);
+        } else {
+          eyeballs.material.color.set(0x0080000);
+        }
+        n++;
+      }
+    }, 500);
 
-    // Setup an animation loop
     const render = () => {
       timeout = requestAnimationFrame(render);
-      update();
       renderer.render(scene, camera);
       gl.endFrameEXP();
     };
@@ -71,15 +85,4 @@ export default function App() {
   };
 
   return <GLView style={{ flex: 1 }} onContextCreate={onContextCreate} />;
-}
-
-class IconMesh extends Mesh {
-  constructor() {
-    super(
-      new BoxBufferGeometry(1.0, 1.0, 1.0),
-      new MeshStandardMaterial({
-        map: new TextureLoader().load(require("./icon.jpg")),
-      })
-    );
-  }
 }
